97e6e2ba9714514084297eb22ef272ecd1bcdd38
Onos Ovueraye <onosovueraye@gmail.com>
2023-06-06 12:02:54 +0100

    created the rails new app for ourneighbourhood

---
+++ db/migrate/20230606105620_add_address_to_users.rb
@@ -0,0 +1,4 @@
+class AddAddressToUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20230606105637_add_neighbourhood_to_users.rb
@@ -0,0 +1,4 @@
+class AddNeighbourhoodToUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
