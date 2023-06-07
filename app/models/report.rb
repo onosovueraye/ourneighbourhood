a9da321dc5b4d35de8a41450f3221ab3aea6eadd
Sam Mayhew <mayhew.sam@yahoo.com>
2023-06-07 12:07:23 +0100

    Added all necessary validations to the models, except for location which I have left as a string

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,4 +4,12 @@ class Report < ApplicationRecord
   has_many :comments
   has_many :tickets
   has_many_attached :photos
+  validates :title, presence: true, length: { maximum: 25 }
+  validates :description, presence: true, length: { maximum: 200 }
+  validates :location, presence: true
+  validates :category, presence: true, inclusion: { in: ['Improvement', 'Repair'] }
+  validates :votes, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :status, presence: true, inclusion: { in: ['Report submitted', 'People Joined', 'Report finished'] }
+
+  attribute :votes, :integer, default: 0
 end
