e023aaff44302a3adced39c1ad63650d10aa2ae6
Aasmund Gravem <aasmund.gravem@gmail.com>
2025-01-07 17:49:52 +0000

    Add data validation checker of long and lat.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,42 +1,47 @@
-import { Controller } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl'
+import { Controller } from '@hotwired/stimulus';
+import mapboxgl from 'mapbox-gl';
 // Connects to data-controller="map"
 export default class extends Controller {
-
   static values = {
     apiKey: String,
-    markers: Array
-  }
+    markers: Array,
+  };
 
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
     console.log(this.markersValue);
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/gravem/cliw677uo014v01r1b6ko2zcf"
-    })
+      style: 'mapbox://styles/gravem/cliw677uo014v01r1b6ko2zcf',
+    });
 
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      if (!marker.lng || !marker.lat) {
+        console.warn('Invalid marker skipped:', marker);
+        return;
+      }
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
 
-      const customMarker = document.createElement("div")
-      customMarker.innerHTML = marker.marker_html
+      const customMarker = document.createElement('div');
+      customMarker.innerHTML = marker.marker_html;
 
       new mapboxgl.Marker(customMarker)
-        .setLngLat([ marker.lng, marker.lat ])
+        .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
-        .addTo(this.map)
-    })
+        .addTo(this.map);
+    });
   }
 
   #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
